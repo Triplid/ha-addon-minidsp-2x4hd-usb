706b284dd8d0fc781ebe09b85e992c77c7849e17
Triplid <58692279+Triplid@users.noreply.github.com>
2025-11-18 19:27:31 +0300

    Update run.sh

--- minidsp_2x4hd_usb/run.sh
+++ minidsp_2x4hd_usb/run.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-exec python3 /app/minidsp_status.py
+#!/usr/bin/env bash
+# Скрипт запуска add-on
+echo "Запуск MiniDSP 2x4HD USB Add-on..."
+# Здесь ваша логика работы с hidapi
+python3 -u main.py
