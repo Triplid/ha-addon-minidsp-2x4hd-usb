892e8a3d3af1bc740db6742a9dd0f316a38e63e3
Triplid <58692279+Triplid@users.noreply.github.com>
2025-11-18 19:48:53 +0300

    Delete minidsp_2x4hd_usb/run.sh

--- minidsp_2x4hd_usb/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Скрипт запуска add-on
-
-# Активируем виртуальную среду с hidapi
-. /usr/src/app/venv/bin/activate
-
-# Здесь запускаем твою программу Python
-# Например:
-python3 /usr/src/app/minidsp_status.py
