ae1911d51b0a163b48674ff289ad55943bf2ea4e
Triplid <58692279+Triplid@users.noreply.github.com>
2025-11-19 14:25:24 +0300

    Delete minidsp_2x4hd_usb/run.sh

--- minidsp_2x4hd_usb/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-# Получаем настройки из конфигурации
-UPDATE_INTERVAL=$(bashio::config 'update_interval')
-
-# Запуск основного скрипта Python с параметрами
-exec python3 /app/minidsp_status.py --interval ${UPDATE_INTERVAL}
